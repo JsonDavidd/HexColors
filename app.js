ba2bd7360b991fb08502112be594903abf78623f
jsondavidd <zambranoyeison21@gmail.com>
2022-05-15 13:46:49 -0400

    Implementing Input element

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// @ts-check
+
 const root = document.getElementById('root')
 
 const button = document.getElementById('button')
-const currentColor = document.getElementById('currentColor')
+const input = document.getElementById('input')
 const color = document.getElementById('color')
 
 const randomInt = (i) => Math.floor(Math.random() * i)
@@ -9,13 +11,15 @@ const randomInt = (i) => Math.floor(Math.random() * i)
 const getRandomHex = (len, random = randomInt) =>
 	Array.from({length: len}, () => random(16).toString(16)).join('')
 
-const changeColor = () => {
-	const hexColor = '#' + getRandomHex(6)
-	currentColor.textContent = hexColor
+const changeColor = (hex = getRandomHex(6)) => {
+	const hexColor = '#' + hex
+	input.placeholder = hex
+	input.value = ''
 	color.style.backgroundColor = hexColor
 }
 
-window.onload = changeColor
+window.onload = () => changeColor()
 
-button.addEventListener('click', () => 
-	changeColor())
+button.addEventListener('click', () => {
+	changeColor(input.value || undefined)
+})
